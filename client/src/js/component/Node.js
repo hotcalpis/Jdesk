308cf4f9ed63f676a5324fbaa3d9d90b923cebdc
hotcalpis <hokkaru3@gmail.com>
2020-08-10 17:01:47 +0900

    axiosでapiを叩いてjsonデータをstateに格納

--- client/src/js/component/Node.js
+++ client/src/js/component/Node.js
@@ -1,9 +1,34 @@
 import React from "react";
+import axios from "axios";
+
+const JDESK_ENDPOINT = 'http://localhost:3010/api/tasks';
 
 export default class Node extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(JDESK_ENDPOINT)
+      .then((results) => {
+        console.dir(results);
+        this.setState({
+          title: results.data.data[0].title
+        });
+      },
+      )
+      .catch(() => {
+        console.log('通信に失敗しました。');
+      });
+  }
+
   render() {
     return (
-      <h1>Node</h1>
+      <h1>{this.state.title}</h1>
     );
   }
 }
