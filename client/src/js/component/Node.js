e0a5dffc837c146626c00d9a36be641ca8cd6a55
hotcalpis <hokkaru3@gmail.com>
2020-08-22 21:24:12 +0900

    react-draggableでのドラッグ移動

--- client/src/js/component/Node.js
+++ client/src/js/component/Node.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Draggable from 'react-draggable';
 
 const JDESK_ENDPOINT = 'http://localhost:3010/api/tasks';
 
@@ -8,72 +9,38 @@ let nodeStyle = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  cursor: "move",
+  // cursor: "move",
   height: "80px",
   width: "80px",
   backgroundColor: "aqua",
 };
 
-const props_left = 8;
-const props_top = 8;
+const props_left = 50;
+const props_top = 50;
 
 export default class Node extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      isDrag: false,
       left: props_left,
       top: props_top,
-      beforeCursorX: 0,
-      beforeCursorY: 0,
     };
-    this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
-  }
-
-  handleMouseDown(e) {
-    this.setState({
-      isDrag: true,
-      left: this.state.left,
-      top: this.state.top,
-      beforeCursorX: e.pageX,
-      beforeCursorY: e.pageY,
-    });
-  }
-
-  handleMouseMove(e) {
-    if (this.state.isDrag == true) {
-      let rect = e.target.getBoundingClientRect()
-      this.setState({
-        left: this.state.left + (e.pageX - this.state.beforeCursorX),
-        top: this.state.top + (e.pageY - this.state.beforeCursorY),
-        beforeCursorX: e.pageX,
-        beforeCursorY: e.pageY,
-      });
-      nodeStyle = {
-        position: "absolute",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        cursor: "move",
-        left: this.state.left,
-        top: this.state.top,
-        height: "80px",
-        width: "80px",
-        backgroundColor: "aqua",
-      };
-    }
-  }
-
-  handleMouseUp() {
-    this.setState({
-      isDrag: false,
-    });
   }
 
   componentDidMount() {
+    nodeStyle = {
+      position: "absolute",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      cursor: "move",
+      left: this.state.left,
+      top: this.state.top,
+      height: "80px",
+      width: "80px",
+      backgroundColor: "aqua",
+    };
     axios
       .get(JDESK_ENDPOINT)
       .then((results) => {
@@ -89,7 +56,9 @@ export default class Node extends React.Component {
 
   render() {
     return (
-      <div className="node" style={nodeStyle} onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>{this.state.title}{this.state.left}{this.state.top}</div>
+      <Draggable>
+        <div className="node" style={nodeStyle}>{this.state.title}</div>
+      </Draggable>
     );
   }
 }
